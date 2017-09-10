34ca225f5b4762fa97fd8905c0578a3aebffc7f8
Felix J. Ogris <fjo@core7.intra.ogris.net>
2017-09-10 23:15:01 +0200

    very first steps

---
+++ imapnotes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# X-Uniform-Type-Identifier: com.apple.mail-note
+
+from Tkinter import *
+
+root = Tk()
+
+w = Label(root, text="Hello, world!")
+w.pack()
+
+b = Button(root, text="Ok")
+b.pack()
+
+root.mainloop()
