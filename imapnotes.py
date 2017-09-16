e9ddefdee568f796f2b1433e6d274d54b4dac602
Felix J. Ogris <fjo@core7.intra.ogris.net>
2017-09-16 23:13:51 +0200

    testtest

--- imapnotes.py
+++ imapnotes.py
@@ -12,6 +12,12 @@ def invokeLater(func):
     eventQueue.put(func)
     root.event_generate("<<invokeLater>>")
 
+def loadNote(*args):
+    index = listbox.curselection()[0]
+    message = notes[len(notes) - index - 1]["message"]
+    text.delete(1.0, Tkinter.END)
+    text.insert(Tkinter.END, message)
+
 config = ConfigParser.SafeConfigParser()
 config.read(os.path.expanduser("~/.imapnotes.ini"))
 
@@ -25,7 +31,7 @@ imap.login(config.get("connection", "user"), config.get("connection", "pass"))
 imap.select("Notes")
 notes_numbers = imap.search(None, "ALL")[1][0].replace(" ", ",") # search returns tuple with list
 notes_list = imap.fetch(notes_numbers, "RFC822") # imap fetch expects comma separated list
-notes = {}
+notes = []
 
 for part in notes_list[1]:
     # imap fetch returns s.th. like: ('OK', [('1 (RFC822 {519}', 'From: ...'), ')'])
@@ -38,7 +44,7 @@ for part in notes_list[1]:
         if not charset is None:
             substring.decode(charset)
         subject += substring
-    notes[num] = {"message": message, "subject": subject}
+    notes.append({"num": num, "message": message, "subject": subject})
 
 root = Tkinter.Tk()
 root.title("imapnotes")
@@ -60,10 +66,11 @@ panedWindow.pack(fill=Tkinter.BOTH, expand=1)
 
 listbox = Tkinter.Listbox(panedWindow)
 panedWindow.add(listbox)
-for num in sorted(notes):
-    listbox.insert(0, notes[num]["subject"])
+for note in notes:
+    listbox.insert(0, note["subject"])
+listbox.bind("<<ListboxSelect>>", loadNote)
 
-text = Tkinter.Text(panedWindow)
+text = Tkinter.Text(panedWindow, undo=True)
 panedWindow.add(text)
 
 eventQueue = Queue.Queue()
