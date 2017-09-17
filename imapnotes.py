b3004ffd637b37070153964431fd65d5c9fabe67
Felix J. Ogris <fjo@core7.intra.ogris.net>
2017-09-17 11:56:30 +0200

    testtest

--- imapnotes.py
+++ imapnotes.py
@@ -2,7 +2,25 @@
 
 # X-Uniform-Type-Identifier: com.apple.mail-note
 
-import Tkinter, threading, Queue, os, ConfigParser, imaplib, email.parser, email.header
+import Tkinter, threading, Queue, os, ConfigParser, imaplib
+import email.parser, email.header, HTMLParser
+
+class HTMLNoteParser(HTMLParser.HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        for k,v in attrs:
+            print "%s: %s:%s" % (tag, k, v)
+        pass
+
+    def handle_endtag(self, tag):
+        if tag in ("br", "div", "p", "h1", "h2", "h3", "h4"):
+            text.insert(Tkinter.END, "\n")
+
+    def handle_startendtag(self, tag, attrs):
+        if tag in ("br", "div", "p", "h1", "h2", "h3", "h4"):
+            text.insert(Tkinter.END, "\n")
+
+    def handle_data(self, data):
+        text.insert(Tkinter.END, data)
 
 def invokeLaterCallback(*args):
     func = eventQueue.get_nowait()
@@ -12,11 +30,25 @@ def invokeLater(func):
     eventQueue.put(func)
     root.event_generate("<<invokeLater>>")
 
-def loadNote(*args):
+def displayMessage(message):
+    if message.is_multipart():
+        for part in message.get_payload():
+            displayMessage(part)
+    else:
+        contenttype = message.get_content_type().lower()
+        body = message.get_payload(decode=True)
+        if contenttype.startswith("text/plain"):
+            text.insert(Tkinter.END, body)
+        elif contenttype.startswith("text/html"):
+            HTMLNoteParser().feed(body)
+        else:
+            text.insert(Tkinter.END, "<cannot display " + contenttype + ">")
+
+def displayNote(*args):
     index = listbox.curselection()[0]
     message = notes[len(notes) - index - 1]["message"]
     text.delete(1.0, Tkinter.END)
-    text.insert(Tkinter.END, message)
+    displayMessage(message)
 
 config = ConfigParser.SafeConfigParser()
 config.read(os.path.expanduser("~/.imapnotes.ini"))
@@ -29,8 +61,10 @@ else:
 imap.login(config.get("connection", "user"), config.get("connection", "pass"))
 
 imap.select("Notes")
-notes_numbers = imap.search(None, "ALL")[1][0].replace(" ", ",") # search returns tuple with list
-notes_list = imap.fetch(notes_numbers, "RFC822") # imap fetch expects comma separated list
+# search returns tuple with list
+notes_numbers = imap.search(None, "ALL")[1][0].replace(" ", ",")
+# imap fetch expects comma separated list
+notes_list = imap.fetch(notes_numbers, "RFC822")
 notes = []
 
 for part in notes_list[1]:
@@ -68,7 +102,7 @@ listbox = Tkinter.Listbox(panedWindow)
 panedWindow.add(listbox)
 for note in notes:
     listbox.insert(0, note["subject"])
-listbox.bind("<<ListboxSelect>>", loadNote)
+listbox.bind("<<ListboxSelect>>", displayNote)
 
 text = Tkinter.Text(panedWindow, undo=True)
 panedWindow.add(text)
